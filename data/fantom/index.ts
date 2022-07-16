a58646e7d513ff921f89f7e6ad4732b8720173f8
djaciel <dj_roses@hotmail.com>
2022-07-15 19:26:52 -0500

    improve file structure

---
+++ data/fantom/index.ts
@@ -0,0 +1,15 @@
+import { pairs } from './pairs';
+import { pairsData } from './pairsData';
+import { paths } from './paths';
+import { singlePaths } from './singlePaths';
+import { routers } from './routers';
+import { tokens } from './tokens';
+
+export {
+  pairs,
+  pairsData,
+  paths,
+  singlePaths,
+  routers,
+  tokens,
+};
---
+++ data/fantom/tokens.ts
@@ -0,0 +1,406 @@
+export const tokens = {
+  USDC: {
+    address: '0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
+    decimals: 6,
+  },
+  WFTM: {
+    address: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
+    decimals: 18,
+  },
+  BOO: {
+    address: '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE',
+    decimals: 18,
+  },
+  SPIRIT: {
+    address: '0x5Cc61A78F164885776AA610fb0FE1257df78E59B',
+    decimals: 18,
+  },
+  LQDR: {
+    address: '0x10b620b2dbAC4Faa7D7FFD71Da486f5D44cd86f9',
+    decimals: 18,
+  },
+  fUSDT: {
+    address: '0x049d68029688eAbF473097a2fC38ef61633A3C7A',
+    decimals: 6,
+  },
+  DAI: {
+    address: '0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E',
+    decimals: 18,
+  },
+  TOMB: {
+    address: '0x6c021Ae822BEa943b2E66552bDe1D2696a53fbB7',
+    decimals: 18,
+  },
+  BTC: {
+    address: '0x321162Cd933E2Be498Cd2267a90534A804051b11',
+    decimals: 8,
+  },
+  DEI: {
+    address: '0xDE12c7959E1a72bbe8a5f7A1dc8f8EeF9Ab011B3',
+    decimals: 18,
+  },
+  ETH: {
+    address: '0x74b23882a30290451A17c44f4F05243b6b58C76d',
+    decimals: 18,
+  },
+  MIM: {
+    address: '0x82f0B8B456c1A451378467398982d4834b6829c1',
+    decimals: 18,
+  },
+  BNB: {
+    address: '0xD67de0e0a0Fd7b15dC8348Bb9BE742F3c5850454',
+    decimals: 18,
+  },
+  LINK: {
+    address: '0xb3654dc3D10Ea7645f8319668E8F54d2574FBdC8',
+    decimals: 18,
+  },
+  TSHARE: {
+    address: '0x4cdF39285D7Ca8eB3f090fDA0C069ba5F4145B37',
+    decimals: 18,
+  },
+  HEC: {
+    address: '0x5C4FDfc5233f935f20D2aDbA572F770c2E377Ab0',
+    decimals: 9,
+  },
+  MIDAS: {
+    address: '0xb37528DA6b4D378305d000a66Ad91bd88E626761',
+    decimals: 18,
+  },
+  miMATIC: {
+    address: '0xfB98B335551a418cD0737375a2ea0ded62Ea213b',
+    decimals: 18,
+  },
+  FRAX: {
+    address: '0xdc301622e621166BD8E82f2cA0A26c13Ad0BE355',
+    decimals: 18,
+  },
+  STG: {
+    address: '0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590',
+    decimals: 18,
+  },
+  sFTMX: {
+    address: '0xd7028092c830b5C8FcE061Af2E593413EbbC1fc1',
+    decimals: 18,
+  },
+  BSHARE: {
+    address: '0x49C290Ff692149A4E16611c694fdED42C954ab7a',
+    decimals: 18,
+  },
+  dKNIGHT: {
+    address: '0x6cc0E0AedbbD3C35283e38668D959F6eb3034856',
+    decimals: 18,
+  },
+  BASED: {
+    address: '0x8D7d3409881b51466B483B11Ea1B8A03cdEd89ae',
+    decimals: 18,
+  },
+  SCREAM: {
+    address: '0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475',
+    decimals: 18,
+  },
+  DEUS: {
+    address: '0xDE5ed76E7c05eC5e4572CfC88d1ACEA165109E44',
+    decimals: 18,
+  },
+  CRV: {
+    address: '0x1E4F97b9f9F913c46F1632781732927B9019C68b',
+    decimals: 18,
+  },
+  SPELL: {
+    address: '0x468003B688943977e6130F4F68F23aad939a1040',
+    decimals: 18,
+  },
+  AVAX: {
+    address: '0x511D35c52a3C244E7b8bd92c0C297755FbD89212',
+    decimals: 18,
+  },
+  TAROT: {
+    address: '0xC5e2B037D30a390e62180970B3aa4E91868764cD',
+    decimals: 18,
+  },
+  YFI: {
+    address: '0x29b0Da86e484E1C0029B56e817912d778aC0EC69',
+    decimals: 18,
+  },
+  SUSHI: {
+    address: '0xae75A438b2E0cB8Bb01Ec1E1e376De11D44477CC',
+    decimals: 18,
+  },
+  TREEB: {
+    address: '0xc60D7067dfBc6f2caf30523a064f416A5Af52963',
+    decimals: 18,
+  },
+  BRUSH: {
+    address: '0x85dec8c4B2680793661bCA91a8F129607571863d',
+    decimals: 18,
+  },
+  GEIST: {
+    address: '0xd8321AA83Fb0a4ECd6348D4577431310A6E0814d',
+    decimals: 18,
+  },
+  PROTO: {
+    address: '0xa23c4e69e5Eaf4500F2f9301717f12B578b948FB',
+    decimals: 18,
+  },
+  MAGIK: {
+    address: '0x87a5C9B60A3aaf1064006FE64285018e50e0d020',
+    decimals: 18,
+  },
+  MULTI: {
+    address: '0x9Fb9a33956351cf4fa040f65A13b835A3C8764E3',
+    decimals: 18,
+  },
+  BIFI: {
+    address: '0xd6070ae98b8069de6B494332d1A1a81B6179D960',
+    decimals: 18,
+  },
+  MATIC: {
+    address: '0x40DF1Ae6074C35047BFF66675488Aa2f9f6384F3',
+    decimals: 18,
+  },
+  pFTM: {
+    address: '0x112dF7E3b4B7Ab424F07319D4E92F41e6608c48B',
+    decimals: 18,
+  },
+  WOO: {
+    address: '0x6626c47c00F1D87902fc13EECfaC3ed06D5E8D8a',
+    decimals: 18,
+  },
+  MARS: {
+    address: '0xBE41772587872A92184873d55B09C6bB6F59f895',
+    decimals: 9,
+  },
+  wsHEC: {
+    address: '0x94CcF60f700146BeA8eF7832820800E2dFa92EdA',
+    decimals: 18,
+  },
+  SD: {
+    address: '0x412a13C109aC30f0dB80AD3Bd1DeFd5D0A6c0Ac6',
+    decimals: 18,
+  },
+  LSHARE: {
+    address: '0xCbE0CA46399Af916784cADF5bCC3aED2052D6C45',
+    decimals: 18,
+  },
+  PAE: {
+    address: '0x8a41f13a4FaE75ca88B1ee726ee9D52B148b0498',
+    decimals: 18,
+  },
+  BEETS: {
+    address: '0xF24Bcf4d1e507740041C9cFd2DddB29585aDCe1e',
+    decimals: 18,
+  },
+  SOUL: {
+    address: '0xe2fb177009FF39F52C0134E8007FA0e4BaAcBd07',
+    decimals: 18,
+  },
+  ALPACA: {
+    address: '0xaD996A45fd2373ed0B10Efa4A8eCB9de445A4302',
+    decimals: 18,
+  },
+  PGK: {
+    address: '0x188a168280589bC3E483d77aae6b4A1d26bD22dC',
+    decimals: 18,
+  },
+  gALCX: {
+    address: '0x70F9fd19f857411b089977E7916c05A0fc477Ac9',
+    decimals: 18,
+  },
+  RING: {
+    address: '0x582423C10c9e83387a96d00A69bA3D11ee47B7b5',
+    decimals: 18,
+  },
+  OXD: {
+    address: '0xc5A9848b9d145965d821AaeC8fA32aaEE026492d',
+    decimals: 18,
+  },
+  SNX: {
+    address: '0x56ee926bD8c72B2d5fa1aF4d9E4Cbb515a1E3Adc',
+    decimals: 18,
+  },
+  OATH: {
+    address: '0x21Ada0D2aC28C3A5Fa3cD2eE30882dA8812279B6',
+    decimals: 18,
+  },
+  YOSHI: {
+    address: '0x3dc57B391262e3aAe37a08D91241f9bA9d58b570',
+    decimals: 18,
+  },
+  FXS: {
+    address: '0x7d016eec9c25232b01F23EF992D98ca97fc2AF5a',
+    decimals: 18,
+  },
+  ANY: {
+    address: '0xdDcb3fFD12750B45d32E084887fdf1aABAb34239',
+    decimals: 18,
+  },
+  TOR: {
+    address: '0x74E23dF9110Aa9eA0b6ff2fAEE01e740CA1c642e',
+    decimals: 18,
+  },
+  FUSD: {
+    address: '0xAd84341756Bf337f5a0164515b1f6F993D194E1f',
+    decimals: 18,
+  },
+  TUSD: {
+    address: '0x9879aBDea01a879644185341F7aF7d8343556B7a',
+    decimals: 18,
+  },
+  ICE: {
+    address: '0xf16e81dce15B08F326220742020379B855B87DF9',
+    decimals: 18,
+  },
+  beFTM: {
+    address: '0x7381eD41F6dE418DdE5e84B55590422a57917886',
+    decimals: 18,
+  },
+  UNIDX: {
+    address: '0x2130d2a1e51112D349cCF78D2a1EE65843ba36e0',
+    decimals: 18,
+  },
+  FLIBERO: {
+    address: '0xC3f069D7439baf6D4D6E9478D9Cc77778E62D147',
+    decimals: 18,
+  },
+  CHARM: {
+    address: '0x248CB87DDA803028dfeaD98101C9465A2fbdA0d4',
+    decimals: 18,
+  },
+  FHM: {
+    address: '0xfa1FBb8Ef55A4855E5688C0eE13aC3f202486286',
+    decimals: 9,
+  },
+  wMEMO: {
+    address: '0xDDc0385169797937066bBd8EF409b5B3c0dFEB52',
+    decimals: 18,
+  },
+  TNODE: {
+    address: '0x7FC5670B2041d34414B0b2178Fc660b1E1faF801',
+    decimals: 18,
+  },
+  SEX: {
+    address: '0xD31Fcd1f7Ba190dBc75354046F6024A9b86014d7',
+    decimals: 18,
+  },
+  COMB: {
+    address: '0xaE45a827625116d6C0C40B5D7359EcF68F8e9AFD',
+    decimals: 18,
+  },
+  '1ART': {
+    address: '0xD3c325848D7c6E29b574Cb0789998b2ff901f17E',
+    decimals: 18,
+  },
+  APE: {
+    address: '0x2d72A97a31Dc920dB03330780d30074626e39C8A',
+    decimals: 18,
+  },
+  FTMO: {
+    address: '0x9bD0611610A0f5133e4dd1bFdd71C5479Ee77f37',
+    decimals: 18,
+  },
+  POWER: {
+    address: '0x131c7afb4E5f5c94A27611f7210dfEc2215E85Ae',
+    decimals: 18,
+  },
+  gOHM: {
+    address: '0x91fa20244Fb509e8289CA630E5db3E9166233FDc',
+    decimals: 18,
+  },
+  PILLS: {
+    address: '0xB66b5D38E183De42F21e92aBcAF3c712dd5d6286',
+    decimals: 18,
+  },
+  MOD: {
+    address: '0xe64B9fd040D1F9D4715C645e0D567EF69958D3D9',
+    decimals: 18,
+  },
+  HND: {
+    address: '0x10010078a54396F62c96dF8532dc2B4847d47ED3',
+    decimals: 18,
+  },
+  WeVE: {
+    address: '0x911da02C1232A3c3E1418B834A311921143B04d7',
+    decimals: 18,
+  },
+  WOOFY: {
+    address: '0xD0660cD418a64a1d44E9214ad8e459324D8157f1',
+    decimals: 12,
+  },
+  NDFI: {
+    address: '0x7Dcd01E3a119C8592A5aEd681cDC9eD163090362',
+    decimals: 18,
+  },
+  ELK: {
+    address: '0xeEeEEb57642040bE42185f49C52F7E9B38f8eeeE',
+    decimals: 18,
+  },
+  FANG: {
+    address: '0x49894fCC07233957c35462cfC3418Ef0CC26129f',
+    decimals: 18,
+  },
+  LIF3: {
+    address: '0xbf60e7414EF09026733c1E7de72E7393888C64DA',
+    decimals: 18,
+  },
+  STAY: {
+    address: '0xA5365f2E77bCe1cb2D42F5c808012C01b1548d3C',
+    decimals: 18,
+  },
+  ZOO: {
+    address: '0x09e145A1D53c0045F41aEEf25D8ff982ae74dD56',
+    decimals: 0,
+  },
+  LIQR: {
+    address: '0x33333ee26a7d02e41c33828B42Fb1E0889143477',
+    decimals: 18,
+  },
+  YEL: {
+    address: '0xD3b71117E6C1558c1553305b44988cd944e97300',
+    decimals: 18,
+  },
+  RISE: {
+    address: '0xC17c30e98541188614dF99239cABD40280810cA3',
+    decimals: 18,
+  },
+  TBOND: {
+    address: '0x24248CD1747348bDC971a5395f4b3cd7feE94ea0',
+    decimals: 18,
+  },
+  IB: {
+    address: '0x00a35FD824c717879BF370E70AC6868b95870Dfb',
+    decimals: 18,
+  },
+  LUX: {
+    address: '0x6671E20b83Ba463F270c8c75dAe57e3Cc246cB2b',
+    decimals: 9,
+  },
+  SOR: {
+    address: '0xEFFd4874AcA3Acd19a24dF3281b5cdAdD823801A',
+    decimals: 18,
+  },
+  RICH: {
+    address: '0x5A272C478609F448C566c7155577f6432fdd8B79',
+    decimals: 9,
+  },
+  RAINI: {
+    address: '0xe83dFaaAfd3310474D917583Ae9633B4f68fB036',
+    decimals: 18,
+  },
+  FAME: {
+    address: '0x904f51a2E7eEaf76aaF0418cbAF0B71149686f4A',
+    decimals: 18,
+  },
+  agEUR: {
+    address: '0x02a2b736F9150d36C0919F3aCEE8BA2A92FBBb40',
+    decimals: 18,
+  },
+  cUSD: {
+    address: '0xE3a486C1903Ea794eED5d5Fa0C9473c7D7708f40',
+    decimals: 18,
+  },
+  PAR: {
+    address: '0x13082681E8CE9bd0aF505912d306403592490Fc7',
+    decimals: 18,
+  },
+}
